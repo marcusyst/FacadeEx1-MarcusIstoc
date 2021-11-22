3b1409d9b84d10bfd34617d1d7315a5c9090d19a
marcusyst <79984370+marcusyst@users.noreply.github.com>
2021-11-22 21:15:16 +0200

    Add files via upload

---
+++ WelcomeToBank.java
@@ -0,0 +1,9 @@
+package example1;
+
+public class WelcomeToBank{
+	
+	public WelcomeToBank(){
+		System.out.println("Welcome to ING!");
+		System.out.println("We are happy to give you your money if we can find it!\n");
+	}
+}
